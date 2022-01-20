adef3b92721a73ed35dc8d35175441ddabe55359
Hijiahmed <hiji.ahmed.rae.js@tuwaiq.edu.sa>
2022-01-20 16:56:28 +0300

    add

---
+++ src/Components/Favorite.js
@@ -0,0 +1,52 @@
+import React,{useState,useEffect} from 'react'
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
+import "./like.css"
+
+export default function Favorite({token}) {
+    const [like, setLike] = useState([]);
+    const history = useHistory();
+    // const {id}=useParams();
+    /////////////////////////////////////
+    useEffect(async () => {
+    if(token){
+        const res = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/Like`, {
+          headers: { authorization: "Bearer " + token },
+        });
+        console.log(res.data);
+        setLike (res.data);
+      }
+      }, []);
+      const deleteLike = async(id,i)=>{
+        const res = await axios.delete(`${process.env.REACT_APP_BACKEND_URL}/Like/${id}`, {
+            headers: { authorization: "Bearer " + token },
+            
+        });
+        const coppyDelete=[...like]
+        coppyDelete.splice(i,1)
+        setLike(coppyDelete)
+      }
+      const gotGame = (id) => {
+        history.push(`/Game/${id}`);
+      };
+      //
+    return (
+        <div className='divLike'>
+             {like.map((elm,i)=>{
+               console.log(elm);
+             return (
+               <div>
+                <div key={i}>
+                  <p>{elm.name}</p>
+                  <img className='imgLike' src={elm.img}  alr="no img" onClick={() => {
+                        gotGame(elm._id);
+                      }} />   
+              </div>
+              <br />
+               <button onClick={()=>{deleteLike(elm._id,i)}}>remove like</button> 
+               </div>        
+             )
+         })}
+        </div>
+    )
+}
---
+++ src/firebase/comp/ProgressBar.js
@@ -0,0 +1,22 @@
+import React, { useEffect } from 'react';
+import useStorage from './UseStorege';
+
+const ProgressBar = ({ file, setFile,setImg }) => {
+  const { progress, url } = useStorage(file);
+
+  useEffect(() => {
+    if (url) {
+      console.log(url);
+      setFile(null);
+      setImg(url)
+    }
+  }, [url, setFile]);
+
+  return (
+    <div>
+      {progress} %
+    </div>
+  );
+} 
+
+export default ProgressBar;
